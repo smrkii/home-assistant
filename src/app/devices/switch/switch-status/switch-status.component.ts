9abcbcd1a04b5ca6426ad2e134636810c2e57e5c
smrkii <zigas112233@gmail.com>
2021-10-31 13:22:09 +0100

    Switch status
    Finnish switch status component.

--- src/app/devices/switch/switch-status/switch-status.component.ts
+++ src/app/devices/switch/switch-status/switch-status.component.ts
@@ -1,8 +1,10 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { NgbActiveModal,NgbCollapse } from '@ng-bootstrap/ng-bootstrap';
 import { ToastrService } from 'ngx-toastr';
 import { SwitchStatus } from '../../../../core/api/devices/shelly1/shelly1.model';
 import { ShellyApiService } from '../../../shared/shelly-api.service';
+import { CollapseDirective } from 'ngx-bootstrap/collapse';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-switch-status',
@@ -12,8 +14,9 @@ import { ShellyApiService } from '../../../shared/shelly-api.service';
 export class SwitchStatusComponent implements OnInit {
   @Input() deviceId: string;
   switchStatus: SwitchStatus;
+  status: boolean = true;
   public isCollapsed = false;
-
+  @ViewChild('f') form: NgForm;
 
   constructor(private shellyApi: ShellyApiService,
     public activeModal: NgbActiveModal,
@@ -26,14 +29,51 @@ export class SwitchStatusComponent implements OnInit {
     this.shellyApi.relayStatus(this.deviceId).subscribe(
       (response) => {
         this.switchStatus = response;
+        this.status = this.switchStatus.isok;
+
+        this.form.form.patchValue({
+          networkGroup:{
+            ssid: this.switchStatus.data.device_status.wifi_sta.ssid,
+            ip: this.switchStatus.data.device_status.wifi_sta.ip,
+            connected: this.switchStatus.data.device_status.wifi_sta.connected,
+            mac: this.switchStatus.data.device_status.mac,
+          },
+          temperatureGroup:{
+            tempStatus: this.switchStatus.data.device_status.temperature_status,
+            temp: this.switchStatus.data.device_status.temperature,
+            overTemp: this.switchStatus.data.device_status.overtemperature
+          },
+          softwareGroup: {
+            softwareStatus: this.switchStatus.data.device_status.update.status,
+            hasUpdate: this.switchStatus.data.device_status.update.has_update,
+            newVersion: this.switchStatus.data.device_status.update.new_version,
+            oldVersion: this.switchStatus.data.device_status.update.old_version
+          }
+        })
 
       },(error)=>{
         this.toastr.error(error.message, 'Error occured!');
       }
     )
 
+
+
   }
 
+  iconCollapse: string = 'icon-arrow-up';
+
+  collapsed(event: any): void {
+    // console.log(event);
+  }
+
+  expanded(event: any): void {
+    // console.log(event);
+  }
+
+  toggleCollapse(): void {
+    this.isCollapsed = !this.isCollapsed;
+    this.iconCollapse = this.isCollapsed ? 'icon-arrow-down' : 'icon-arrow-up';
+  }
 
 
 }
--- src/app/devices/switch/switch.module.ts
+++ src/app/devices/switch/switch.module.ts
@@ -5,6 +5,8 @@ import { RouterModule } from '@angular/router';
 import { SwitchComponent } from './switch.component';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { SwitchStatusComponent } from './switch-status/switch-status.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -15,7 +17,9 @@ import { SwitchStatusComponent } from './switch-status/switch-status.component';
     RouterModule,
     CommonModule,
     SwitchRoutingModule,
-    BsDropdownModule
+    BsDropdownModule,
+    NgbModule,
+    FormsModule
   ],
   exports:[SwitchComponent]
 })
