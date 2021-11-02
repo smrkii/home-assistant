37eaef9b627bc4b44762a7626aeba4c9bf0ba3d9
smrkii <zigas112233@gmail.com>
2021-11-02 18:22:58 +0100

    Revert "Fix prod enviroment file"
    This reverts commit e63efc025afe425b5a9acda1e7ad7d7af607a5d2.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  shelly_auth_key: 'NzYxMmN1aWQ555C6F1FC950DDB8B24056C43449C1A8698E997DB2B7CE110D860BAD2A85A76CA2BD1CED9EF06B81',
+  auth_key: 'NzYxMmN1aWQ555C6F1FC950DDB8B24056C43449C1A8698E997DB2B7CE110D860BAD2A85A76CA2BD1CED9EF06B81',
   base_url: 'https://shelly-29-eu.shelly.cloud',
   apiKey: 'AIzaSyB-aVA25EWKMMnsYkgdi-wkwYX6-AaGqr4',
   endpoints:[
